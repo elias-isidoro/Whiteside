6c450ea82a031b1f26c354577ac3ace9f3d2bfb2
Dean <137676224+elias-isidoro@users.noreply.github.com>
2023-07-18 15:20:57 +0800

    set dynamic to force dynamic

--- src/app/api/display/categories/all/route.ts
+++ src/app/api/display/categories/all/route.ts
@@ -1,12 +1,14 @@
 import { db } from "@/lib/db";
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET () {
   try {
     
     const categories = await db.category.findMany();
 
-    return NextResponse.json({ categories, time: new Date().toISOString() })
+    return NextResponse.json({ categories })
 
   }catch(error){
     return new Response('Could not fetch categories', {status: 500})
--- src/app/api/display/products/all/route.ts
+++ src/app/api/display/products/all/route.ts
@@ -1,11 +1,13 @@
 import { db } from "@/lib/db";
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET () {
   try {
     const products = await db.product.findMany({ include:{ variants: true } });
 
-    return NextResponse.json({ products, time: new Date().toISOString() })
+    return NextResponse.json({ products })
 
   }catch(error){
     return new Response('Could not fetch products', {status: 500})
