6e21ebe9ceda8ccf95e58ca65da6ce25459d38ad
Dean <137676224+elias-isidoro@users.noreply.github.com>
2023-07-19 18:21:31 +0800

    try catch error

--- src/app/api/payment/link/route.ts
+++ src/app/api/payment/link/route.ts
@@ -1,35 +1,59 @@
-import { createPaymongoLink } from '@/lib/paymongo';
 import { CreatePaymongoLinkValidator, CreatePaymongoLinkPayload } from '@/lib/validators/paymongo';
+import axios from 'axios';
 import { NextResponse } from 'next/server';
+
+interface PaymongoLink {
+  data: {
+    id: string;
+    type: string;
+    attributes: {
+      amount: number;
+      archived: boolean;
+      currency: string;
+      description: string;
+      livemode: boolean;
+      fee: number;
+      remarks: string;
+      status: string;
+      tax_amount: number | null;
+      taxes: any[];
+      checkout_url: string;
+      reference_number: string;
+      created_at: number;
+      updated_at: number;
+      payments: any[];
+    };
+  };
+}
 
 export async function POST(req: Request) {
   try {
+    
     const body = await req.json();
 
     const payload: CreatePaymongoLinkPayload = CreatePaymongoLinkValidator.parse(body);
     
-    // Call the createPaymongoLink function
     try{
-      const { data: { attributes: { checkout_url = '/' } } } = await createPaymongoLink(payload);
+      const { data: { attributes: { checkout_url = '/' } } }: PaymongoLink = await axios.post(
+        'https://api.paymongo.com/v1/links',
+        { data: { attributes: payload } },
+        {
+          headers: { Accept: 'application/json' },
+          auth: { username: process.env.PAYMONGO_SECRET_KEY!, password: '' },
+        }
+      )
+  
       return NextResponse.json({ link: checkout_url }, { status: 200 });
+
     }catch (err) {
 
-      if(err instanceof Error){
-        return NextResponse.json({ link: err.message }, { status: 200 });
-      }else{
-        return { data: { attributes: { checkout_url: 'Failed to axios Paymongo link' } } }
-      }
-
+      return NextResponse.json({ link: 'failed to request for a link' }, { status: 200 });
       
     }
 
-    
   } catch (err) {
-    if(err instanceof Error){
-      return NextResponse.json({ link: err.message }, { status: 500 });
-    }else{
-      return { data: { attributes: { checkout_url: 'Failed at beginning of creation of Paymongo link' } } }
-    }
+    
+    return NextResponse.json({ link: 'Invalid payload' }, { status: 500 });
 
   }
 }
