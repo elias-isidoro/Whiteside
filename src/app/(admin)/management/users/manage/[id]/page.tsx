72efe6ce8a062b41c68ff213f92fd7d8164cd12e
Dean <137676224+elias-isidoro@users.noreply.github.com>
2023-08-04 19:41:25 +0800

    Update page.tsx

---
+++ src/app/(admin)/management/users/manage/[id]/page.tsx
@@ -0,0 +1,19 @@
+
+import checkAuthorization from '@/lib/authorizer'
+
+interface Props {
+  params:{
+    id: string
+  }
+}
+
+const page = async ({params: {id}}: Props) => {
+
+  await checkAuthorization()
+
+  return (
+    <>id</>
+  )
+}
+
+export default page
