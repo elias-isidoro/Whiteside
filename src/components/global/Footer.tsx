e28b69c1da27057e11f7d5ee2a7787163bf983ee
Dean <dean.cuyco27@gmail.com>
2023-08-09 21:50:27 +0800

    fix footer for smaller screens

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -3,14 +3,16 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="mt-10 flex flex-col gap-5 text-xs">
+      <div className="mt-10 flex flex-col gap-5 text-[11px] min-[300px]:text-xs">
         <hr/>
         <div className="flex flex-col gap-2">
           <div className="flex flex-row">
             <p>&copy; {new Date().getFullYear()} Dean Kudou. All rights reserved.</p>
           </div>
-          <div className="flex flex-row">
-            <p>Contact me: naed221@gmail.com | Phone: +63 915-451-1087</p>
+          <div className="flex flex-col gap-2 min-[400px]:flex-row">
+            <p>Email: naed221@gmail.com</p>
+            <p className="hidden min-[400px]:block">|</p>
+            <p>Phone: +63 915-451-1087</p>
           </div>
         </div>
       </div>
