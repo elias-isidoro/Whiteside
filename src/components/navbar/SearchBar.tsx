4f2ee3e24952a1376cc17398452c265baea430a7
Dean <dean.cuyco27@gmail.com>
2023-08-11 19:09:25 +0800

    only allow searches in storefront

--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -9,18 +9,20 @@ const SearchBar = () => {
   const { searchInput, setSearchInput } = useSearchStore()
 
   const pathname = usePathname()
+
+  const isStoreFront = pathname === '/'
   const isDashboard = pathname.split('/').includes('dashboard')
   const isManagement = pathname.split('/').includes('management')
   
   return(
     <div className='relative flex h-full flex-grow items-center pr-2'>
       <Input 
-      disabled={isDashboard || isManagement}
+      disabled={!isStoreFront}
       value={searchInput}
       onChange={(e)=>{ setSearchInput(e.target.value) }}
       placeholder="Search store..."
       className='border border-gray-400 h-[35px] rounded-sm pl-[27px] text-xs'/>
-
+      
       <div
       className={`absolute left-0  ml-[2px] flex items-center justify-center aspect-square h-full w-[30px] ${isDashboard||isManagement?'text-gray-300':'text-gray-500'}`}>
         <SearchIcon className="h-4 w-4"/>
